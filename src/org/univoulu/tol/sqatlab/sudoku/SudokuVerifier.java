dbf66f74f2eb15a065c1af025cde4e1429925e05
SteCorn <s.cornacchia7@studenti.uniba.it>
2016-11-28 12:03:42 +0100

    Fine

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -1,9 +1,194 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
 public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
 		// returns 1 if the candidate solution is correct
-		return 0;
+		int validity = 0;
+		
+			if (isnumber(candidateSolution) == false){
+				validity = -1;
+				}else if (isonceingrid(candidateSolution) == false){
+					validity = -2;
+				}else if (isonceinrow(candidateSolution) == false){
+					validity = -3;
+				}else if (isonceincolumn(candidateSolution) == false){
+				validity = -4;
+				}
+		
+		return validity;
+	
+	}
+	
+
+	public boolean isnumber(String candidateSolution){
+		boolean result=true;
+		int i=0;
+		
+		char[] numberstring;
+		numberstring=candidateSolution.toCharArray();
+		
+		for(i=0;(i<numberstring.length && result);i++){
+			if(Character.isDigit(numberstring[i])==false){
+				result=false;
+			}
+		}
+		return result;
+	}
+	
+	
+	public boolean isonceingrid(String candidateSolution){
+		
+		boolean result=true;
+		String[] grid= new String[9];
+		grid=creategrid(candidateSolution);
+		int i=0;
+		
+		for(i=0;i<grid.length;i++){
+			if(singlenumber(grid[i])==false){
+				result=false;
+			}
+		}
+		return result;
+	}
+
+	public boolean isonceinrow(String candidateSolution) {
+		boolean result=true;
+		
+		String[] row= new String[9];
+		row=createrow(candidateSolution);
+		int i=0;
+		
+		for(i=0;i<row.length;i++){
+			if(singlenumber(row[i])==false){
+				result=false;
+			}
+		}
+		return result;
+	}
+
+
+	public boolean isonceincolumn(String candidateSolution) {
+		boolean result=true;
+		
+		String[] column= new String[9];
+		column=createcolumn(candidateSolution);
+		int i=0;
+		
+		for(i=0;i<column.length;i++){
+			if(singlenumber(column[i])==false){
+				result=false;
+			}
+		}
+		return result;
+	}
+
+
+	private String[] creategrid(String candidateSolution) {
+		String[] grid= new String[9];
+		
+		int i,j,k,jump,count=0;
+		
+		for(i=0;i<9;i++){
+			grid[i]="";
+		}
+		
+		char[] numberstring;
+		numberstring=candidateSolution.toCharArray();
+		jump=0;
+		for(k=0;k<9;k++){			
+			for(i=jump;i<numberstring.length && count<3;i+=9){
+				count++;
+				for(j=i;j<(i+3);j++){
+					grid[k]=grid[k]+numberstring[j];
+					
+				}
+			}
+			count=0;
+			jump=jump+3;
+			
+			if(jump==9||jump==36||jump==63){
+				jump=jump+18;
+			}
+				
+		}
+			
+		return grid;
+	}
+	
+	
+	private String[] createrow(String candidateSolution) {
+		String[] row= new String[9];
+		
+		int i,j,k,jump,count=0;
+		
+		for(i=0;i<9;i++){
+			row[i]="";
+		}
+		
+		char[] numberstring;
+		numberstring=candidateSolution.toCharArray();
+		jump=0;
+		for(k=0;k<9;k++){			
+			for(i=jump;i<numberstring.length && count<9;i+=9){
+				for(j=i;j<(i+9);j++){
+					row[k]=row[k]+numberstring[j];
+					count++;
+				}
+			}
+			count=0;
+			jump=jump+9;			
+		}
+			
+		return row;
+	}
+
+	private String[] createcolumn(String candidateSolution) {
+		String[] column= new String[9];
+		
+		int i,j,k,jump,count=0;
+		
+		for(i=0;i<9;i++){
+			column[i]="";
+		}
+		
+		char[] numberstring;
+		numberstring=candidateSolution.toCharArray();
+		jump=0;
+		for(k=0;k<9;k++){			
+			for(i=jump;i<9 && count<9;i++){
+				for(j=i;j<numberstring.length;j+=9){
+					column[k]=column[k]+numberstring[j];
+					count++;
+				}
+			}
+			count=0;
+			jump++;			
+		}
+			
+		return column;
+	}
+
+	
+	
+
+	public boolean singlenumber(String string) {
+		boolean result=true;
+		int i,j=0;
+		
+		char[] numberstring;
+		numberstring=string.toCharArray();
+		for(i=0;i<numberstring.length;i++){
+			for(j=i+1;j<numberstring.length && result;j++){
+				if(numberstring[i]==numberstring[j]){
+					result=false;
+				}
+			}
+		}
+		
+		return result;
 	}
 }
+
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -5,10 +5,70 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
-
+	
+	SudokuVerifier sudoku = new SudokuVerifier();
+	String correct="417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	String incorrect="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	String incString_1 = "123456789 ";
+	String incString_2 = " 123456789";
+	String incString_3 = "12345b6789";
+	String incString_4 = "12345 6789";
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void verifytest(){
+		assertEquals(0,sudoku.verify(correct));
+		assertEquals(-2,sudoku.verify(incorrect));
 	}
-
+	
+	@Test
+	public void isnumerictest() {
+		//assertEquals(0,sudoku.verify(correct));
+		//assertEquals(0,sudoku.verify(incorrect));
+				
+		
+		assertEquals(true, sudoku.isnumber(correct));
+		assertEquals(false, sudoku.isnumber(incString_1));
+		assertEquals(false, sudoku.isnumber(incString_2));
+		assertEquals(false, sudoku.isnumber(incString_3));
+		assertEquals(false, sudoku.isnumber(incString_4));
+	}
+	
+	@Test
+	public void isonceingrid(){
+		
+		assertEquals(true, sudoku.isonceingrid(correct));
+		assertEquals(false, sudoku.isonceingrid(incorrect));
+		
+	}
+	
+	@Test
+	public void isonceinrow(){
+		
+		String incorow="123456789912345678891234567789123456678912345567891234456789123345678912234567899";
+		assertEquals(true, sudoku.isonceinrow(correct));
+		assertEquals(true, sudoku.isonceinrow(incorrect));
+		assertEquals(false, sudoku.isonceinrow(incorow));
+	}
+	
+	@Test
+	public void isonceincolumn(){
+		
+		String incolumn="123456789912345678891234567789123456678912345567891234456789123345678911234567891";
+		
+		assertEquals(true, sudoku.isonceincolumn(correct));
+		assertEquals(true, sudoku.isonceincolumn(incorrect));
+		assertEquals(false, sudoku.isonceincolumn(incolumn));
+	}
+	
+	@Test
+	public void singlenumber(){
+		
+		String cor1="0123456789";
+		String inc1="1234567893";
+		
+		assertEquals(true, sudoku.singlenumber(cor1));
+		assertEquals(false, sudoku.singlenumber(inc1));
+		
+	}
+	
 }
